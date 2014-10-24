af55ac5130858ab365ec0c14963407da61326d30
Luis Zárate <luis.zaratemontero@ucr.ac.cr>
2014-10-23 22:59:00 -0600

    include translation

--- accordion_presentation/cms_plugins.py
+++ accordion_presentation/cms_plugins.py
@@ -11,7 +11,7 @@ from .models import  PresentationModel, PresentationAccordion, FieldPresentation
 
 class AccordionPlugin(CMSPluginBase):
     model = PresentationAccordion
-    name = _('Accordion presentation')
+    name = _('Horizontal Accordion')
     #module = _('Accordion')
     render_template = 'accordion_presentation/accordion.html'
     allow_children = True
@@ -26,8 +26,10 @@ class AccordionPlugin(CMSPluginBase):
 
 class PresentationPlugin(CMSPluginBase):
     model = PresentationModel                # Model where data about this plugin is saved
-    name = _("Presentation Plugin")                 # Name of the plugin
+    name = _("Field in the accordion")                 # Name of the plugin
     render_template = "accordion_presentation/plugin.html"   # template to render the plugin with
+    allow_children = False
+    parent_classes = ['AccordionPlugin']
 
     def render(self, context, instance, placeholder):
         context.update({'instance':instance,
@@ -37,7 +39,7 @@ class PresentationPlugin(CMSPluginBase):
 
 class FieldPlugin(CMSPluginBase):
     model = FieldPresentation
-    name = _('Field accordion presentation')
+    name = _('Full color column')
     render_template = 'accordion_presentation/field.html'
     allow_children = False
     
